9d2d724a86839024b4fa123963030c2028e7f8bb
Ivan Jacques da Silva <ivanjacques.1997@gmail.com>
2023-10-16 09:32:24 -0300

    Atualização barra de pesquisa

--- navegador.py
+++ navegador.py
@@ -1,25 +1,38 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QLineEdit, QAction
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
 
-class SimpleWebBrowser(QMainWindow):
+class ImprovedWebBrowser(QMainWindow):
     def __init__(self):
         super().__init__()
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("http://www.google.com"))  # URL inicial
+        self.browser.setUrl(QUrl("http://www.google.com"))
 
         self.setCentralWidget(self.browser)
 
+        # Adicione uma barra de pesquisa e um botão "Pesquisar"
+        navtb = QToolBar("Navigation")
+        self.addToolBar(navtb)
+
+        self.urlbar = QLineEdit()
+        self.urlbar.returnPressed.connect(self.navigate_to_url)
+        navtb.addWidget(self.urlbar)
+
+        search_action = QAction("Pesquisar", self)
+        search_action.setStatusTip("Pesquisar na web")
+        search_action.triggered.connect(self.navigate_to_url)
+        navtb.addAction(search_action)
+
+    def navigate_to_url(self):
+        q = QUrl(self.urlbar.text())
+        if q.scheme() == "":
+            q.setScheme("http")
+        self.browser.setUrl(q)
+
 def main():
     app = QApplication(sys.argv)
-    
-    # Definir uma fonte padrão para o aplicativo (no exemplo, "Arial")
-    font = app.font()
-    font.setFamily("Arial")
-    app.setFont(font)
-
-    window = SimpleWebBrowser()
+    window = ImprovedWebBrowser()
     window.show()
     sys.exit(app.exec_())
 
